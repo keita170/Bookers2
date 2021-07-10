da5668a453362e0a1bf3c553c9132dae13d94bcb
keita170 <noinoi0565@gmail.com>
2021-07-10 03:09:45 +0000

    retouching

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7044ac86eb213eaad5346a186f41e56932654cc9ceb0c5762779d0770d8a1f063ea77e5520ba6b8796ea693577d762432c3d409d2ae1e99f8c390adf25c8e83e'
