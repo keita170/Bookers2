901773975a594cf98beafe8e4e206225bc50998d
keita170 <noinoi0565@gmail.com>
2021-07-27 03:03:16 +0000

    search

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'homes#top'
 
   get 'home/about' => 'homes#about'
+  
+  get 'searchs/search' => 'searchs#search'
 
   resources :users, only: [:show, :index, :edit, :update] do
     resources :relationships, only: [:create, :destroy]
